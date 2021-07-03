5fdfebf33f55fe8daf61b091a74ffb295047ac8f
William Jesús Covarrubias Ramos <Skr13@outlook.com>
2021-07-02 21:38:30 -0700

    2

--- src/app.ts
+++ src/app.ts
@@ -26,25 +26,8 @@ const app = express();
 app.set("port", process.env.PORT || 4000);
 
 //middle
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://come-back-front.vercel.app/",
-];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-};
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
