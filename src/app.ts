e0eab73d1ad4b10d82efcde3b29b3edf24a97428
William Jesús Covarrubias Ramos <Skr13@outlook.com>
2021-07-02 21:29:01 -0700

    test

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,17 @@ const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
 app.use(morgan("dev"));
-app.use(cors(options));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
